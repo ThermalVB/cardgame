116776296a18fb860bde2b68d3f845b7b9ec49dc
Peter Felton <peteraminfelton@gmail.com>
2017-03-08 14:47:22 -0800

    Fixing merging conflicts.

--- deckOfCards/BlackJack.cs
+++ deckOfCards/BlackJack.cs
@@ -11,11 +11,7 @@ namespace DeckOfCards
                 player.DrawFrom(deck);
                 dealer.DrawFrom(deck);
             }
-<<<<<<< HEAD
-            if (player.HandValue() == 21){
-=======
             if (player.handValue() == 21){
->>>>>>> 4d3da4bec1a680bca8ebe0537bfd024b8072710a
                 return "playerWin";
             }
             // if (dealer.upcard == "Ace"){
@@ -25,36 +21,22 @@ namespace DeckOfCards
         }
         public string hit(Player player,Deck deck){
             player.DrawFrom(deck);
-<<<<<<< HEAD
-            if (player.HandValue() == 21){
-                return "playerWin";
-            }
-            if (player.HandValue() > 21){
-=======
             if (player.handValue() == 21){
                 return "playerWin";
             }
             if (player.handValue() > 21){
->>>>>>> 4d3da4bec1a680bca8ebe0537bfd024b8072710a
                 return "playerBust";
             }
             return "continue";
         }
         public string stay(Player dealer,Player player,Deck deck){
-            dealer.dealerLogic();
+            dealer.dealerLogic(deck);
             deck.Reset();
             deck.Shuffle();
-<<<<<<< HEAD
-            if (dealer.HandValue() == 21 || (dealer.HandValue() > player.HandValue())){
-                return "dealerWin";
-            }
-            if (dealer.HandValue() > 21){
-=======
-            if (dealer.handValue() == 21 || (dealer.handValue > player.handvalue())){
+            if (dealer.handValue() == 21 || (dealer.handValue() > player.handValue())){
                 return "dealerWin";
             }
             if (dealer.handValue() > 21){
->>>>>>> 4d3da4bec1a680bca8ebe0537bfd024b8072710a
                 return "dealerBust";
             }
             return "playerWin";
--- deckOfCards/Player.cs
+++ deckOfCards/Player.cs
@@ -1,15 +1,12 @@
 using System.Collections.Generic;
 
 namespace DeckOfCards {
+
+    
     public class Player {
         public string name;
-<<<<<<< HEAD
         public List<Card> hand;
-=======
-        private List<Card> hand;
-        public BlackJack dealer  = new BlackJack();
->>>>>>> 4d3da4bec1a680bca8ebe0537bfd024b8072710a
-
+        public BlackJack blackJacker = new BlackJack();
         public int chipTotal;
 
         public Player(string n = "Player") {
@@ -27,33 +24,17 @@ namespace DeckOfCards {
             hand.RemoveAt(idx);
             return temp;
         }
-<<<<<<< HEAD
-
-        public void dealerLogic(){
-
-                int value = HandValue();
-
-                if(value < 16){
-                    
-                    //   BlackJack.hit();
-                }
-        }
-
-          public int HandValue(){
-            
-=======
         // this method will make sure score will greater than 16,
         public void dealerLogic(Deck currentDeck){
                 int value = this.handValue();
                 
                 while(value < 16){
                     
-                    dealer.hit(this, currentDeck);
+                    blackJacker.hit(this, currentDeck);
                     value = this.handValue();
                 }
         }
           public int handValue(){
->>>>>>> 4d3da4bec1a680bca8ebe0537bfd024b8072710a
             int value =0;
              foreach(Card val in hand){
                  if(val.val > 10 ){
