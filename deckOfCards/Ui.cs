06c890dc1ed6a84f8b90e5cb394ceea80b111eff
Peter Felton <peteraminfelton@gmail.com>
2017-03-08 15:14:20 -0800

    Changes to ui logic and other minor changes.

--- deckOfCards/Ui.cs
+++ deckOfCards/Ui.cs
@@ -1,18 +1,27 @@
 using System;
+using System.Collections.Generic;
 
-namespace DeckOfCards {
-    public class Ui {
+namespace DeckOfCards
+{
+    public class Ui
+    {
         Deck myDeck = new Deck();
         Looks typing = new Looks(60);
         BlackJack logic = new BlackJack();
         Player myPlayer;
+
+        List<Player> playerList = new List<Player>();
         Player myDealer = new Player("The Dealer");
-        
-        public Ui(){
+        string move;
+        string returner;
+
+        public Ui()
+        {
             resetScreen();
         }
 
-        public void resetScreen(){
+        public void resetScreen()
+        {
             typing.TopLine();
             typing.BlankLine();
             typing.CenterLine("Welcome to Black Jack!");
@@ -21,21 +30,31 @@ namespace DeckOfCards {
             typing.BlankLine();
             typing.BottomLine();
             myPlayer = new Player(typing.User_Input("Name"));
-            if(logic.startGame(myPlayer, myDealer, myDeck) == "playerWin") {
+            playerList.Add(myDealer);
+            playerList.Add(myPlayer);
+
+            if (logic.startGame(myPlayer, myDealer, myDeck) == "playerWin")
+            {
                 winScreen("playerWin");
-            } else {
+            }
+            else
+            {
                 playScreen("start");
             }
         }
 
-        public void winScreen(string winCondition){
+        public void winScreen(string winCondition)
+        {
             typing.TopLine();
             typing.BlankLine();
-            if(winCondition == "playerWin"){
+            if (winCondition == "playerWin")
+            {
                 typing.CenterLine("Congratulations!");
                 typing.BlankLine(2);
                 typing.CenterLine("You won!");
-            } else {
+            }
+            else
+            {
                 typing.CenterLine("The game has ended.");
                 typing.BlankLine(2);
                 typing.CenterLine("...and we don't know why...");
@@ -44,23 +63,52 @@ namespace DeckOfCards {
             typing.BottomLine();
         }
 
-        public void playScreen(string isStart = "not"){
-            typing.TopLine();
-            typing.BlankLine();
-            if(isStart == "start"){
+        public void playScreen(string isStart = "not")
+        {
+
+            if (isStart == "start")
+            {
+                typing.TopLine();
+                typing.BlankLine();
                 typing.CenterLine("Let's Play!");
                 typing.CenterLine("-- The Dealer has dealt. --");
                 typing.BlankLine(2);
             }
-            typing.CenterLine("Your Turn:");
-            typing.BlankLine();
-            typing.CreateLine("Your Cards:");
-            foreach(Card card in myPlayer.hand){
-                typing.CreateLine(" - " + card.ToString());
+            foreach (Player player in playerList)
+            {
+                if (player.name != "The Dealer")
+                {
+                    typing.TopLine();
+                    typing.BlankLine();
+                    typing.CenterLine("Your Turn:");
+                    typing.BlankLine();
+                    typing.CreateLine("Your Cards:");
+                    foreach (Card card in player.hand)
+                    {
+                        typing.CreateLine(" - " + card.ToString());
+                    }
+                    typing.BlankLine();
+                    typing.CreateLine("Do you want to 'hit' or 'stay' ?");
+                    typing.BottomLine();
+                    move = typing.User_Input("Input");
+                    while (move != "hit" || move != "stay")
+                    {
+                        typing.TopLine();
+                        typing.CenterLine("Sorry, I didn't understand that. Please type it again.");
+                        typing.BottomLine();
+                        move = typing.User_Input("Input");
+                    }
+                    if (move == "hit")
+                    {
+                        returner = logic.hit(player, myDeck);
+                    }
+                    else if (move == "stay")
+                    {
+                        returner = logic.stay(myDealer, player, myDeck);
+
+                    }
+                }
             }
-            typing.BlankLine();
-            typing.CreateLine("Do you want to 'hit' or 'stay' ?");
-            typing.BottomLine();
         }
 
     }
