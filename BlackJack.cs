0dbdadb1ab318b9d79d60b8f9ab423579423b691
ThermalVB <wyatt.horejsi@gmail.com>
2017-03-08 14:49:36 -0800

    Add files via upload
    adding get/set

--- BlackJack.cs
+++ BlackJack.cs
@@ -1,17 +1,26 @@
 namespace DeckOfCards
 {
     public class BlackJack {
+        
+        public Player myPlayer { get; set; }
+        public Player myDealer { get; set; }
+        public Deck myDeck { get; set; }
 
         public int deckCount = 0;
 
         public string startGame(Player player,Player dealer,Deck deck){
 
+            
+            Player myPlayer = player; 
+            Player  myDealer = dealer; 
+            Deck myDeck = deck;
+
             for (int i = 0; i < 2; i++)
             {
-                player.DrawFrom(deck);
-                dealer.DrawFrom(deck);
+                myPlayer.DrawFrom(deck);
+                myDealer.DrawFrom(deck);
             }
-            if (player.handValue == 21){
+            if (myPlayer.handValue == 21){
                 return "playerWin";
             }
             // if (dealer.upcard == "Ace"){
@@ -19,28 +28,27 @@ namespace DeckOfCards
             // }
             return "continue";
         }
-        public string hit(Player player,Deck deck){
-            player.DrawFrom(deck)
-            if (player.handValue == 21){
+        public string hit(Player playerTemp){
+            playerTemp.DrawFrom(myDeck);
+            if (playerTemp.handValue == 21){
                 return "playerWin";
             }
-            if (player.handValue > 21){
+            if (playerTemp.handValue > 21){
                 return "playerBust";
             }
             return "continue";
         }
-        public string stay(Player dealer,Player player,Deck deck){
-            dealer.dealerLogic();
-            deck.Reset();
-            deck.Shuffle();
-            if (dealer.handValue == 21 || (dealer.handValue > player.handvalue)){
+        public string stay(){
+            myDealer.dealerLogic();
+            myDeck.Reset();
+            myDeck.Shuffle();
+            if (myDealer.handValue == 21 || (myDealer.handValue > myPlayer.handvalue)){
                 return "dealerWin";
             }
-            if (dealer.handValue > 21){
+            if (myDealer.handValue > 21){
                 return "dealerBust";
             }
             return "playerWin";
             }
         }
     }
-}
